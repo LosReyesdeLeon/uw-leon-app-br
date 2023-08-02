d07f20c948322875fd978b63370ca08b480c228e
tommy <nat.nguyen2001@gmail.com>
2023-08-02 13:23:01 -0700

    I basically just installed a fork with miguel. Now I'm going to hopefully scrap my code from the last project and see if it works out.

--- app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/main/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/main/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import edu.tacoma.uw.set.css.uwleonappbr.R;
 
 public class MainActivity extends AppCompatActivity {
-
+//this is made by nathan just checking in
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
