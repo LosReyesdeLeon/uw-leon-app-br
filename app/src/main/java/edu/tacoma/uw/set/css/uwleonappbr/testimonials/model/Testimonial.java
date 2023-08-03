4e36027d07d3c0503e8d9d55fa5c20552fd89bae
Devin <devin.peevy@outlook.com>
2023-08-03 09:07:28 -0700

    I updated the Testimonial page with a few JavaDocs and added the getters.

--- app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/model/Testimonial.java
+++ app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/model/Testimonial.java
@@ -1,11 +1,11 @@
 package edu.tacoma.uw.set.css.uwleonappbr.testimonials.model;
 
 /**
+ * This is the object class that holds all the information included in a testimonial submitted by
+ * the student. Each field corresponds to a column in the Testimonials table in mysql database.
  * @author Devin Peevy
  */
 public class Testimonial {
-
-    // Example
 
     public final static String SEATTLE = "Seattle";
 
@@ -33,6 +33,14 @@ public class Testimonial {
 
     private String testimonialContent;
 
+    /**
+     * This is how a Testimonial is going to be constructed by the SubmitStudentInfoFragment.
+     * @param studentID 7 digit UW student id number.
+     * @param studentName The first and last name of the student.
+     * @param studentCampus The UW campus the student normally attends: SEATTLE, TACOMA, or BOTHELL.
+     * @param programSeason The season of the student's program: SPRING, SUMMER, FALL, or WINTER.
+     * @param programYear 4 digit year of the student's program.
+     */
     public Testimonial(int studentID,
                        String studentName,
                        String studentCampus,
@@ -46,11 +54,39 @@ public class Testimonial {
         this.testimonialContent = null;
     }
 
+    /**
+     * The SubmitTestimonialFragment will then complete construction using this method.
+     * @param testimonialTitle A short sentence/phrase to describe the student's experience MAX: 50.
+     * @param testimonialContent The content of the testimonial MAX: 1,000.
+     */
     public void addTestimonial(String testimonialTitle,
                                String testimonialContent) {
         this.testimonialTitle = testimonialTitle;
         this.testimonialContent = testimonialContent;
     }
 
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentCampus() {
+        return studentCampus;
+    }
+
+    public String getProgramSeason() {
+        return programSeason;
+    }
+
+    public String getTestimonialTitle() {
+        return testimonialTitle;
+    }
+
+    public String getTestimonialContent() {
+        return testimonialContent;
+    }
 }
 
