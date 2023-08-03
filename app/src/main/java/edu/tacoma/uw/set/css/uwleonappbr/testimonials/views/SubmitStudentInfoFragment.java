b12ddf9fff9dbdfb87a7d57b5e48663dfb2f5f3d
Devin <devin.peevy@outlook.com>
2023-08-03 13:48:48 -0700

    Done some error checking in the SubmitStudentInfoFragment.java. Couple minor related changes. Renamed the field studentSeason in Testimonial to studentQuarter.

--- app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/views/SubmitStudentInfoFragment.java
+++ app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/views/SubmitStudentInfoFragment.java
@@ -2,23 +2,126 @@ package edu.tacoma.uw.set.css.uwleonappbr.testimonials.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import edu.tacoma.uw.set.css.uwleonappbr.R;
+import java.time.Year;
+import java.util.Calendar;
+
+import edu.tacoma.uw.set.css.uwleonappbr.databinding.FragmentSubmitStudentInfoBinding;
+import edu.tacoma.uw.set.css.uwleonappbr.testimonials.model.Testimonial;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SubmitStudentInfoFragment extends Fragment {
 
+    private FragmentSubmitStudentInfoBinding mBinding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_submit_student_info, container, false);
+        mBinding = FragmentSubmitStudentInfoBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mBinding.submitStudentInfoButton.setOnClickListener(button -> processStudentInfo());
+    }
+
+    private void processStudentInfo() {
+        String name = mBinding.editNameField.getText().toString();
+        int studentID = Integer.parseInt(mBinding.editStudentIDField.getText().toString());
+        String campus = "";
+        String season = "";
+        if (getSelectedCampus() != null && getSelectedSeason() != null) {
+            campus = getSelectedCampus();
+            season = getSelectedSeason();
+        } else {
+            // Toasts will already be shown for these two. Just cancel so we don't submit an
+            // error.
+            return;
+        }
+        int year = Integer.parseInt(mBinding.editYearField.getText().toString());
+        String major = mBinding.editMajorField.getText().toString();
+        if (!hasErrors(name, studentID, year, major)) {
+            // TODO: First I have to set up the navigation, and then I will program this to move on
+            // to the next fragment.
+        }
+        // If it has errors, the hasErrors method will print the toast. We just want to stop so we
+        // don't process invalid information.
+    }
+
+    private boolean hasErrors(String name, int studentID, int year, String major) {
+        if (name.isEmpty()) {
+            Toast.makeText(getContext(),
+                    "Name field may not be left blank.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (studentID < 999999) {
+            Toast.makeText(getContext(),
+                    "Student ID number must be 7 digits long.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (year < 2010) {
+            Toast.makeText(getContext(),
+                    "The UW León center was not open at that time.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (year > Calendar.getInstance().get(Calendar.YEAR)) {
+            Toast.makeText(getContext(),
+                    "You cannot submit a testimonial from the future.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (major.isEmpty()) {
+            Toast.makeText(getContext(),
+                    "Major field may not be left blank.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
+    private String getSelectedCampus() {
+        if (mBinding.seattleButton.isSelected()) {
+            return Testimonial.SEATTLE;
+        } else if (mBinding.tacomaButton.isSelected()) {
+            return Testimonial.TACOMA;
+        } else if (mBinding.bothellButton.isSelected()) {
+            return Testimonial.BOTHELL;
+        } else {
+            // Nothing is selected.
+            Toast.makeText(getContext(),
+                    "You must select a campus.", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
+    private String getSelectedSeason() {
+        if (mBinding.springButton.isSelected()) {
+            return Testimonial.SPRING;
+        } else if (mBinding.summerButton.isSelected()) {
+            return Testimonial.SUMMER;
+        } else if (mBinding.fallButton.isSelected()) {
+            return Testimonial.FALL;
+        } else if (mBinding.winterButton.isSelected()) {
+            return Testimonial.WINTER;
+        } else {
+            // Nothing is selected.
+            Toast.makeText(getContext(), "You must select a Season for your program quarter.",
+                            Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
 }
--- app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/views/SubmitTestimonialFragment.java
+++ app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/views/SubmitTestimonialFragment.java
@@ -2,11 +2,14 @@ package edu.tacoma.uw.set.css.uwleonappbr.testimonials.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import edu.tacoma.uw.set.css.uwleonappbr.R;
 
@@ -20,5 +23,10 @@ public class SubmitTestimonialFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_submit_testimonial, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
