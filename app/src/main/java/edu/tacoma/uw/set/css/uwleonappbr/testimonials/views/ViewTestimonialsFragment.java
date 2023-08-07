5c780f5d6556aadb44adfdd5a93912d4985d60a7
Devin <devin.peevy@outlook.com>
2023-08-07 16:47:48 -0700

    View Testimonials Fragment and the layout are completed and working.

--- app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/views/ViewTestimonialsFragment.java
+++ app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/testimonials/views/ViewTestimonialsFragment.java
@@ -2,23 +2,50 @@ package edu.tacoma.uw.set.css.uwleonappbr.testimonials.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.tacoma.uw.set.css.uwleonappbr.R;
+import edu.tacoma.uw.set.css.uwleonappbr.databinding.FragmentViewTestimonialsBinding;
+import edu.tacoma.uw.set.css.uwleonappbr.testimonials.model.TestimonialViewModel;
+import edu.tacoma.uw.set.css.uwleonappbr.testimonials.model.TestimonialsRecyclerViewAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ViewTestimonialsFragment extends Fragment {
 
+    private TestimonialViewModel testimonialViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        testimonialViewModel = new ViewModelProvider(getActivity()).get(TestimonialViewModel.class);
+        testimonialViewModel.getTestimonialsFromDatabase();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_testimonials, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        @NonNull FragmentViewTestimonialsBinding binding = FragmentViewTestimonialsBinding.bind(getView());
+
+        testimonialViewModel.addTestimonialListObserver(getViewLifecycleOwner(), testimonials -> {
+            if (!testimonials.isEmpty()) {
+                binding.layoutRoot.setAdapter(new TestimonialsRecyclerViewAdapter(testimonials));
+            }
+        });
     }
 }
