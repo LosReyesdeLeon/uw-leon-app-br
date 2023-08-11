6f09a65e90b95dd319cc2b72f712665e5aa3b75b
Matthew <mattburgess155@gmail.com>
2023-08-11 13:09:14 -0700

    Added book appointment button to link to main study abroad page. Fixed colorscheme to UW layout. Fixed scaling issues with spacers.

--- app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/info/ScholarshipInfoFragment.java
+++ app/src/main/java/edu/tacoma/uw/set/css/uwleonappbr/info/ScholarshipInfoFragment.java
@@ -1,5 +1,7 @@
 package edu.tacoma.uw.set.css.uwleonappbr.info;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import edu.tacoma.uw.set.css.uwleonappbr.R;
 
@@ -19,6 +22,19 @@ public class ScholarshipInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scholarship_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_scholarship_info, container, false);
+
+        Button bookAppointmentButton = view.findViewById(R.id.book_appointment_button);
+
+        bookAppointmentButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String url = "https://www.tacoma.uw.edu/oga/sa";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        return view;
     }
 }
